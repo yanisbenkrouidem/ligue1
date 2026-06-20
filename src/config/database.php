64247be82ebfbd4b52c855a1383455839f380211
yanisbenkrouidem <yanis.benkrouidem@gmail.com>
2026-06-20 21:16:59 +0200

    Fix PDO SSL constant deprecation warning

--- src/config/database.php
+++ src/config/database.php
@@ -18,7 +18,6 @@ $dsn = "mysql:host=$host;port=$port;dbname=$dbname;charset=utf8";
 $options = [
     PDO::ATTR_ERRMODE            => PDO::ERRMODE_EXCEPTION,
     PDO::ATTR_DEFAULT_FETCH_MODE => PDO::FETCH_ASSOC,
-    PDO::MYSQL_ATTR_SSL_VERIFY_SERVER_CERT => false,
 ];
 
 try {
